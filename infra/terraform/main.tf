ce38826a3d0f97f44f4585c9ffb4b18440a597c3
BirdyBigBird <74187014+BirdyBigBird@users.noreply.github.com>
2026-03-12 23:24:16 +0000

    Add Azure Storage Container for tfstate

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,3 +21,9 @@ resource "azurerm_storage_account" "platform_storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.platform_storage.name
+  container_access_type = "private"
+}
