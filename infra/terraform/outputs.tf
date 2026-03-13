49eb442c0f93b7cf65a6f19bcbb1d53e56f18d97
BirdyBigBird <74187014+BirdyBigBird@users.noreply.github.com>
2026-03-13 00:42:05 +0000

    Refactor outputs to use modules for resources

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.foundryops_rg.name
+  value = module.resource_group.resource_group_name
+}
+
+output "resource_group_location" {
+  value = module.resource_group.resource_group_location
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.platform_storage.name
-}
-
-output "tfstate_container_name" {
-  value = azurerm_storage_container.tfstate.name
+  value = module.storage_account.storage_account_name
 }
