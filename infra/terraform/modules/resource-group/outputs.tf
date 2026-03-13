1ed24ab586abf54cf53b27c92d4b28aa3e3e3b75
BirdyBigBird <74187014+BirdyBigBird@users.noreply.github.com>
2026-03-13 00:36:33 +0000

    Add outputs for resource group name and location

--- infra/terraform/modules/resource-group/outputs.tf
+++ infra/terraform/modules/resource-group/outputs.tf
@@ -1,3 +1,7 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.this.name
+output "resource_group_name" {
+  value = azurerm_resource_group.this.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.this.location
 }
